19df2dc9e76da6aba80de13b7b6488df9e772eb1
Brian Reisman <brianreisman@protonmail.com>
2020-11-02 17:24:47 -0500

    spinner complete

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -37,24 +37,35 @@ STEP 4:
   Do you remember the operator we use to do "not"?
 */
 
-import React from 'react'; /* STEP 0 */
+import React, { useState } from 'react'; /* STEP 0 */
 
 export default function Spinner() {
 /* STEP 1 */
+  const [spinnerOn, setSpinnerOn] = useState(true);
 
-  const toggleSpinner = () => {
+  const toggleSpinner = (e) => {
+    setSpinnerOn(!spinnerOn);
+    e.target.value = '';
+    // setSpinnerOn(false);
   /* STEP 4 */
   };
 
   return (
     <div className='widget-spinner container'>
       <h2>Spinner</h2>
-      {
-        true && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
-      }
+      {spinnerOn && <div id='spinner' className='spinner'>--+--</div>} {/* STEP 2 */}
       <button id='toggleSpinner' onClick={toggleSpinner}>
-        Hide Spinner {/* STEP 3 */}
+        {spinnerOn == true ? 'Hide' : 'Show'} spinner {/* STEP 3 */}
       </button>
     </div>
   );
 }
+
+// STEP 2:/=
+  // This is called a logical expression. If the expressions on both sides of the '&&' are truthy,
+  // the one on the right becomes the value of the whole line. If an expression on either side of the '&&'
+  // is falsy, the one on the left becomes the value of the whole line. It's a neat little trick to render
+  // a React element (in this case the spinner) conditionally: only if the variable on the left is truthy.
+// 
+  // Replace the hard-coded 'true' with the variable that keeps track of whether spinner is on or not.
+// 
